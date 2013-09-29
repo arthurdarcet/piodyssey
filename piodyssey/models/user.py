bc52ba8dcaef6d65273ca5cf2d22cb7e2482c33d
Arthur Darcet <arthur.darcet@m4x.org>
2013-09-30 00:06:15 +0200

    Cleanup the user creation form

--- piodyssey/models/user.py
+++ piodyssey/models/user.py
@@ -72,59 +72,51 @@ class User(AbstractBaseUser):
         return True
 
 
-class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
 class UserChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField()
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=False)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput, required=False)
 
     class Meta:
         model = User
 
     def clean_password(self):
-        return self.initial["password"]
+        return self.initial.get('password', None)
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password1
+
+    def save(self, commit=True):
+        user = super(UserChangeForm, self).save(commit=False)
+        if self.cleaned_data['password1']:
+            user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
 
 
 class UserAdmin(DjUserAdmin):
     form = UserChangeForm
-    add_form = UserCreationForm
+    add_form = UserChangeForm
 
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('is_staff',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'password1', 'password2')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'date_of_birth', 'phone')}),
         ('Parents', {'fields': ('parent_phone', 'parent_email')}),
         ('Permissions', {'fields': ('is_staff',)}),
         ('Important dates', {'fields': ('last_login',)}),
     )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'date_of_birth', 'password1', 'password2')}
-        ),
+            'fields': ('email', 'first_name', 'last_name')
+        }),
     )
     search_fields = ('email',)
     ordering = ('email',)
