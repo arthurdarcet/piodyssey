c9a146a0c95b00a77450297f704b54a9c88b6c88
Arthur Darcet <arthur.darcet@m4x.org>
2013-09-29 15:34:21 +0200

    Login form

--- piodyssey/models/user.py
+++ piodyssey/models/user.py
@@ -2,6 +2,7 @@ from django import forms
 from django.db import models
 
 from django.contrib import admin
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as DjUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
